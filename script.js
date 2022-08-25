0fafb22cbcadc21e157833b71ca8dd00aad23462
Kelly Nelson <kelnel86@gmail.com>
2022-08-25 16:29:00 -0500

    creates breedImg and noImgBreed arrays

--- script.js
+++ script.js
@@ -150,8 +150,74 @@ let names = [
   "Turkish Van",
   "York Chocolate",
 ];
-let breedImg = [];
-let noImgBreed = [];
+let breedImg = [
+  "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
+  "https://cdn2.thecatapi.com/images/ozEvzdVM-.jpg",
+  "https://cdn2.thecatapi.com/images/hBXicehMA.jpg",
+  "https://cdn2.thecatapi.com/images/xnsqonbjW.jpg",
+  "https://cdn2.thecatapi.com/images/JFPROfGtQ.jpg",
+  "https://cdn2.thecatapi.com/images/8D--jCd21.jpg",
+  "https://cdn2.thecatapi.com/images/k71ULYfRr.jpg",
+  "https://cdn2.thecatapi.com/images/_6x-3TiCA.jpg",
+  "https://cdn2.thecatapi.com/images/13MkvUreZ.jpg",
+  "https://cdn2.thecatapi.com/images/5AdhMjeEu.jpg",
+  "https://cdn2.thecatapi.com/images/O3btzLlsO.png",
+  "https://cdn2.thecatapi.com/images/HOrX5gwLS.jpg",
+  "https://cdn2.thecatapi.com/images/5iYq9NmT1.jpg",
+  "https://cdn2.thecatapi.com/images/7isAO4Cav.jpg",
+  "https://cdn2.thecatapi.com/images/s4wQfYoEk.jpg",
+  "https://cdn2.thecatapi.com/images/4lXnnfxac.jpg",
+  "https://cdn2.thecatapi.com/images/jvg3XfEdC.jpg",
+  "https://cdn2.thecatapi.com/images/B1ERTmgph.jpg",
+  "https://cdn2.thecatapi.com/images/TR-5nAd_S.jpg",
+  "https://cdn2.thecatapi.com/images/j6oFGLpRG.jpg",
+  "https://cdn2.thecatapi.com/images/vJ3lEYgXr.jpg",
+  "https://cdn2.thecatapi.com/images/IFXsxmXLm.jpg",
+  "https://cdn2.thecatapi.com/images/oSpqGyUDS.jpg",
+  "https://cdn2.thecatapi.com/images/unX21IBVB.jpg",
+  "https://cdn2.thecatapi.com/images/3dbtapCWM.jpg",
+  "https://cdn2.thecatapi.com/images/tJbzb7FKo.jpg",
+  "https://cdn2.thecatapi.com/images/4RzEwvyzz.png",
+  "https://cdn2.thecatapi.com/images/3KG57GfMW.jpg",
+  "https://cdn2.thecatapi.com/images/BQMSld0A0.jpg",
+  "https://cdn2.thecatapi.com/images/TuSyTkt2n.jpg",
+  "https://cdn2.thecatapi.com/images/YnPrYEmfe.jpg",
+  "https://cdn2.thecatapi.com/images/njK25knLH.jpg",
+  "https://cdn2.thecatapi.com/images/CDhOtM-Ig.jpg",
+  "https://cdn2.thecatapi.com/images/-tm9-znzl.jpg",
+  "https://cdn2.thecatapi.com/images/xoI_EpOKe.jpg",
+  "https://cdn2.thecatapi.com/images/165ok6ESN.jpg",
+  "https://cdn2.thecatapi.com/images/DbwiefiaY.png",
+  "https://cdn2.thecatapi.com/images/NZpO4pU56M.jpg",
+  "https://cdn2.thecatapi.com/images/aKbsEYjSl.jpg",
+  "https://cdn2.thecatapi.com/images/OOD3VXAQn.jpg",
+  "https://cdn2.thecatapi.com/images/fhYh2PDcC.jpg",
+  "https://cdn2.thecatapi.com/images/j5cVSqLer.jpg",
+  "https://cdn2.thecatapi.com/images/OGTWqNNOt.jpg",
+  "https://cdn2.thecatapi.com/images/06dgGmEOV.jpg",
+  "https://cdn2.thecatapi.com/images/JAx-08Y0n.jpg",
+  "https://cdn2.thecatapi.com/images/LutjkZJpH.jpg",
+  "https://cdn2.thecatapi.com/images/-Zfz5z2jK.jpg",
+  "https://cdn2.thecatapi.com/images/z7fJRNeN6.jpg",
+  "https://cdn2.thecatapi.com/images/SMuZx-bFM.jpg",
+  "https://cdn2.thecatapi.com/images/oGefY4YoG.jpg",
+  "https://cdn2.thecatapi.com/images/Rhj-JsTLP.jpg",
+  "https://cdn2.thecatapi.com/images/a8nIYvs6S.jpg",
+  "https://cdn2.thecatapi.com/images/o9t0LDcsa.jpg",
+  "https://cdn2.thecatapi.com/images/II9dOZmrw.jpg",
+  "https://cdn2.thecatapi.com/images/ai6Jps4sx.jpg",
+  "https://cdn2.thecatapi.com/images/3bkZAjRh1.jpg",
+  "https://cdn2.thecatapi.com/images/Qtncp2nRe.jpg",
+  "https://cdn2.thecatapi.com/images/MK-sYESvO.jpg",
+  "https://cdn2.thecatapi.com/images/EPF2ejNS0.jpg",
+  "https://cdn2.thecatapi.com/images/BDb8ZXb1v.jpg",
+  "https://cdn2.thecatapi.com/images/KBroiVNCM.jpg",
+  "https://cdn2.thecatapi.com/images/O3F3_S1XN.jpg",
+  "https://cdn2.thecatapi.com/images/7CGV6WVXq.jpg",
+  "https://cdn2.thecatapi.com/images/sxIXJax6h.jpg",
+  "https://cdn2.thecatapi.com/images/0SxW2SQ_S.jpg",
+];
+let noImgBreed = ["ebur", "mala"];
 //let breedsWithImgs = [];
 
 //////////////
@@ -176,10 +242,10 @@ function getBreedsLoop() {
   );
 }
 getBreedsLoop();
-console.log(breedId);
-console.log(breedImg);
-console.log(noImgBreed);
-console.log(names);
+// console.log(breedId);
+// console.log(breedImg);
+// console.log(noImgBreed);
+// console.log(names);
 //console.log(breeds);
 
 // let breedsWithImgs = names.filter((breed) => {
